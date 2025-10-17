6888cab8c1adc479a6e39f3eeb56f6c21bb4fbeb
Thai Ngoc Qui <155046579+TnQuj@users.noreply.github.com>
2025-10-17 14:41:37 +0700

    Add files via upload

---
+++ Before_ExtractVariable_03/BannerRenderer.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Before_ExtractVariable_03
+{
+    class BannerRenderer
+    {
+        private string platform;
+        private string browser;
+        private int resize;
+        private bool initialized;
+
+        public BannerRenderer(string platform, string browser, int resize, bool initialized)
+        {
+            this.platform = platform;
+            this.browser = browser;
+            this.resize = resize;
+            this.initialized = initialized;
+        }
+
+        private bool WasInitialized()
+        {
+            return initialized;
+        }
+
+        public void RenderBanner()
+        {
+            bool isMacOs = platform.ToUpper().Contains("MAC");
+            bool isIE = browser.ToUpper().Contains("IE");
+            bool wasResized = resize > 0;
+
+            if (isMacOs && isIE && WasInitialized() && wasResized)
+            {
+                Console.WriteLine("Rendering banner for MacOS IE...");
+            }
+            else
+            {
+                Console.WriteLine("Conditions not met, skipping banner.");
+            }
+        }
+    }
+
+}
---
+++ Before_ExtractVariable_03/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace Before_ExtractVariable_03
+{
+
+    class Program
+    {
+        static void Main()
+        {
+            BannerRenderer banner = new BannerRenderer("Mac", "IE", 10, true);
+            banner.RenderBanner();
+
+            BannerRenderer banner2 = new BannerRenderer("Windows", "Chrome", 10, true);
+            banner2.RenderBanner();
+
+            Console.ReadKey();
+        }
+    }
+}
