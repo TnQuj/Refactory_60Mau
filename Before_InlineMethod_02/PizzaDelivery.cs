e92a5e25e147b7b0c0580ceb0e4dd71471097d4e
Thai Ngoc Qui <155046579+TnQuj@users.noreply.github.com>
2025-10-17 14:33:02 +0700

    Add files via upload

---
+++ Before_InlineMethod_02/PizzaDelivery.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Before_InlineMethod_02
+{
+
+    class PizzaDelivery
+    {
+        private int numberOfLateDeliveries;
+
+        public PizzaDelivery(int lateDeliveries)
+        {
+            this.numberOfLateDeliveries = lateDeliveries;
+        }
+
+        // Trước khi refactor
+        public int GetRating()
+        {
+            return MoreThanFiveLateDeliveries() ? 2 : 1;
+        }
+
+        private bool MoreThanFiveLateDeliveries()
+        {
+            return numberOfLateDeliveries > 5;
+        }
+    }
+
+}
---
+++ Before_InlineMethod_02/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+namespace Before_InlineMethod_02
+{
+    class Program
+    {
+        static void Main()
+        {
+            PizzaDelivery pizza1 = new PizzaDelivery(3);
+            PizzaDelivery pizza2 = new PizzaDelivery(8);
+
+            Console.WriteLine($"Pizza 1 rating: {pizza1.GetRating()}");
+            Console.WriteLine($"Pizza 2 rating: {pizza2.GetRating()}");
+
+            Console.ReadKey();
+        }
+
+    }
+}
