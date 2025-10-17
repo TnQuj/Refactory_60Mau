4e0d5718f2dae98c2d3c87295889de168ba3497a
Thai Ngoc Qui <155046579+TnQuj@users.noreply.github.com>
2025-10-17 14:31:36 +0700

    Add files via upload

---
+++ Before_ExtractMethod_01/Customers.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Before_ExtractMethod_01
+{
+    class Customer
+    {
+        private string name;
+        private List<double> purchases = new List<double>();
+
+        public Customer(string name)
+        {
+            this.name = name;
+        }
+
+        public void AddPurchase(double amount)
+        {
+            purchases.Add(amount);
+        }
+
+        // Tính tổng số tiền nợ (outstanding)
+        public double GetOutstanding()
+        {
+            double total = 0;
+            foreach (var amount in purchases)
+            {
+                total += amount;
+            }
+            return total;
+        }
+
+        // In tiêu đề
+        private void PrintBanner()
+        {
+            Console.WriteLine("**************************");
+            Console.WriteLine("**** Customer Owes ******");
+            Console.WriteLine("**************************");
+        }
+
+        // PHIÊN BẢN TRƯỚC KHI REFACTOR
+        public void PrintOwing()
+        {
+            this.PrintBanner();
+
+            // Print details.
+            Console.WriteLine("name: " + this.name);
+            Console.WriteLine("amount: " + this.GetOutstanding());
+        }
+    }
+}
---
+++ Before_ExtractMethod_01/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Before_ExtractMethod_01
+{
+
+    class Program
+    {
+        static void Main()
+        {
+            // Tạo khách hàng
+            Customer customer = new Customer("Alice");
+
+            // Thêm các khoản nợ
+            customer.AddPurchase(120.50);
+            customer.AddPurchase(89.90);
+            customer.AddPurchase(50.00);
+
+            // In thông tin nợ
+            customer.PrintOwing();
+
+            Console.ReadKey();
+        }
+    }
+}
