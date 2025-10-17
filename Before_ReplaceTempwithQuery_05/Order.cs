d57acfc4c8e9e059867fc9ebcee47221ec1b5fd2
Thai Ngoc Qui <155046579+TnQuj@users.noreply.github.com>
2025-10-17 14:58:41 +0700

    Add files via upload

---
+++ Before_ReplaceTempwithQuery_05/Order.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Before_Replace_Temp_with_Query_05
+{
+    class Order
+    {
+        private int quantity;
+        private double itemPrice;
+
+        public Order(int quantity, double itemPrice)
+        {
+            this.quantity = quantity;
+            this.itemPrice = itemPrice;
+        }
+
+        public double CalculateTotal()
+        {
+            double basePrice = quantity * itemPrice;
+
+            if (basePrice > 1000)
+            {
+                return basePrice * 0.95;  // giảm 5%
+            }
+            else
+            {
+                return basePrice * 0.98;  // giảm 2%
+            }
+        }
+
+        public void PrintInvoice()
+        {
+            Console.WriteLine($"Số lượng: {quantity}");
+            Console.WriteLine($"Giá gốc: {quantity * itemPrice}");
+            Console.WriteLine($"Tổng thanh toán: {CalculateTotal()}");
+        }
+    }
+}
