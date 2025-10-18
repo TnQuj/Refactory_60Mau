ccc37cce69ef53041814733288cacf8321176fa7
Thai Ngoc Qui <155046579+TnQuj@users.noreply.github.com>
2025-10-18 15:17:01 +0700

    Add files via upload

---
+++ 18_SelfEncapsulateField/After_SelfEncapsulateField/Program.cs
@@ -0,0 +1,25 @@
+﻿class Range
+{
+    private int low, high;
+
+    public Range(int low, int high)
+    {
+        this.low = low;
+        this.high = high;
+    }
+
+    public bool Includes(int value)
+    {
+        return value >= GetLow() && value <= GetHigh();
+    }
+
+    public int GetLow()
+    {
+        return low;
+    }
+
+    public int GetHigh()
+    {
+        return high;
+    }
+}
---
+++ 18_SelfEncapsulateField/Before_SelfEncapsulateField/Program.cs
@@ -0,0 +1,15 @@
+﻿class Range
+{
+    private int low, high;
+
+    public Range(int low, int high)
+    {
+        this.low = low;
+        this.high = high;
+    }
+
+    public bool Includes(int value)
+    {
+        return value >= low && value <= high;
+    }
+}
