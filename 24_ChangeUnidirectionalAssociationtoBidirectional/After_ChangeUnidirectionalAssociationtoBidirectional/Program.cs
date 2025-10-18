761d8d11e2904c71c4521bf9cafce6d1230b9b3d
Thai Ngoc Qui <155046579+TnQuj@users.noreply.github.com>
2025-10-18 15:36:56 +0700

    Add files via upload

---
+++ 24_ChangeUnidirectionalAssociationtoBidirectional/After_ChangeUnidirectionalAssociationtoBidirectional/Program.cs
@@ -0,0 +1,36 @@
+﻿public class Customer
+{
+    public string Id { get; }
+    public string Name { get; }
+
+    public Customer(string id, string name)
+    {
+        Id = id;
+        Name = name;
+    }
+
+    // Hai khách hàng được xem là bằng nhau nếu có cùng Id và Name
+    public override bool Equals(object? obj)
+    {
+        if (obj is not Customer other) return false;
+        return Id == other.Id && Name == other.Name;
+    }
+
+    public override int GetHashCode()
+    {
+        return HashCode.Combine(Id, Name);
+    }
+}
+
+public class Order
+{
+    public int OrderId { get; }
+    public Customer Customer { get; }
+
+    public Order(int orderId, Customer customer)
+    {
+        OrderId = orderId;
+        Customer = customer;
+    }
+}
+
---
+++ 24_ChangeUnidirectionalAssociationtoBidirectional/Before_ChangeUnidirectionalAssociationtoBidirectional/Program.cs
@@ -0,0 +1,25 @@
+﻿
+public class Customer
+{
+    public string Id { get; }
+    public string Name { get; }
+
+    public Customer(string id, string name)
+    {
+        Id = id;
+        Name = name;
+    }
+}
+
+public class Order
+{
+    public int OrderId { get; }
+    public Customer Customer { get; }
+
+    public Order(int orderId, Customer customer)
+    {
+        OrderId = orderId;
+        Customer = customer;
+    }
+}
+
