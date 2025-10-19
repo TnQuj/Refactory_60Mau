4d78a6b33e128168d3eed913058e8ba9ae23c3b2
Thai Ngoc Qui <155046579+TnQuj@users.noreply.github.com>
2025-10-19 23:33:56 +0700

    Add files via upload

---
+++ 32_Remove Control Flag/Remove Control Flag problem/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+
+class Program
+{
+    static void Main()
+    {
+        List<int> numbers = new List<int> { 2, 4, 6, 8, 10 };
+        int target = 6;
+        bool found = false;
+
+        foreach (int num in numbers)
+        {
+            if (num == target)
+            {
+                found = true;
+            }
+
+            if (found)
+            {
+                Console.WriteLine("Found target: " + target);
+                break;
+            }
+        }
+    }
+}
